77f73038ddd78fa58619d11151faf8805629d2fd
novelty <novelty@purelymail.com>
2025-12-13 22:21:34 -0800

    Basic stuff

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,8 @@
 module.exports = function (eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("src/css");
+    eleventyConfig.addPassthroughCopy("src/images");
+    eleventyConfig.addPassthroughCopy("src/music");
+
     return {
       dir: {
         input: "src",
